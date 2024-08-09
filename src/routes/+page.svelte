83a41b8c24541e1185274b0d91d7fb73fa7be96d
gloriohm <gruud@tutanota.com>
2024-08-09 10:12:05 +0200

    added copy all button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	let nsDate = '';
 	let isoRef = '';
 	let enRef = '';
+	let copySuccess = false; // State to show/hide success message
 
 	function handleSubmit(event) {
 		event.preventDefault();
@@ -10,6 +11,21 @@
 		nsDate = '';
 		isoRef = '';
 		enRef = '';
+	}
+
+	function copyToClipboard() {
+		const textToCopy = `
+			${enDate} ble internasjonal standard ${isoRef} adoptert som europeisk standard ${enRef}.\n\n${nsDate} ble europeisk standard ${enRef} fastsatt som Norsk Standard NS-${isoRef}. Engelsk versjon ble utgitt ${nsDate}.\n\nVed eventuell uoverensstemmelse, vil PDF-versjon legges til grunn.
+		`.trim();
+		// Copy the text to clipboard
+		navigator.clipboard.writeText(textToCopy.trim())
+			.then(() => {
+				copySuccess = true; // Show success message
+				setTimeout(() => copySuccess = false, 2000); // Hide after 2 seconds
+			})
+			.catch(err => {
+				console.error("Failed to copy text: ", err);
+			});
 	}
 </script>
 
@@ -21,7 +37,7 @@
 			<input type="text" bind:value={enDate} class="outline outline-2" />
 		</label>
 		<label class="flex flex-col">
-			NS dato:
+			NS-ISO dato:
 			<input type="text" bind:value={nsDate} class="outline outline-2" />
 		</label>
 	</div>
@@ -35,9 +51,9 @@
 			<input type="text" bind:value={enRef} class="outline outline-2" />
 		</label>
 	</div>
-	<button type="submit" class="my-5 outline outline-2 bg-slate-500 max-w-36 py-2">
-		Tøm skjema
-	</button>
+		<button type="submit" class="my-5 outline outline-2 bg-slate-500 max-w-36 py-2">
+			Tøm skjema
+		</button>
 </form>
 
 
@@ -52,4 +68,10 @@
 		</p>
 		<p>Ved eventuell uoverensstemmelse, vil PDF-versjon legges til grunn.</p>
 	</div>
+	<button on:click={copyToClipboard} class="mt-3 p-2 bg-blue-500 text-white rounded">
+		Kopier til utklippstavle
+	</button>
+	{#if copySuccess}
+		<p class="mt-2 text-green-500">Teksten er kopiert til utklippstavlen!</p>
+	{/if}
 {/if}
